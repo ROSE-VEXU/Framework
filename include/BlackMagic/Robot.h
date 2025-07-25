bd38213b23d6502d9357ab416c1145d4975ed56a
AVesich <75646434+AVesich@users.noreply.github.com>
2025-07-25 00:15:45 -0600

    made driveMode virtual without a constructor, improved keyword use in Robot, made Subsystem abstract and updated withSubsystem/Robot

--- include/BlackMagic/Robot.h
+++ include/BlackMagic/Robot.h
@@ -17,14 +17,21 @@ public:
     static std::unique_ptr<Robot> currentReference; // required to run auton & driver control due to vex needing function ptr (can't pass 'this')
     static void auton(void);
     static void driverControl(void);
-    Robot& withSubsystem(Subsystem subsystem);
+
+    template<typename SubsystemT>
+    Robot& withSubsystem(SubsystemT&& subsystem) {
+        static_assert(std::is_base_of<Subsystem, std::decay_t<SubsystemT>>::value, "Robot.withSubsystem requires a Subsystem type object to work.");
+        subsystems.push_back(std::make_unique<std::decay_t<SubsystemT>>(std::forward<SubsystemT>(subsystem)));
+        return *this;
+    }
+
     // Robot& withAutonomousRoutine(const std::string& name, void(*)(void) routine);
     // Robot& withAutonomousRoutine(const std::string& name, std::function<void()>* routine);
-    Robot& withAutonomousRoutine(const std::string& name, std::function<void()> routine);
+    Robot& withAutonomousRoutine(const std::string& name, const std::function<void()>& routine);
 private:
     vex::competition& competitionController;
     AutonomousSelector autoSelector;
-    std::vector<Subsystem> subsystems;
+    std::vector<std::unique_ptr<Subsystem>> subsystems;
 };
 
 };
--- src/BlackMagic/DriveMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "vex.h"
-
-namespace BlackMagic {
-
-DriveMode::DriveMode() {
-
-}
-
-};
