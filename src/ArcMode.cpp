f3b8b68dd4c1c2b338f6591f18afdc99c95d1477
AVesich <75646434+AVesich@users.noreply.github.com>
2026-02-05 17:04:34 -0500

    added arc mode

---
+++ src/ArcMode.cpp
@@ -0,0 +1,55 @@
+#include "vex.h"
+
+namespace BlackMagic {
+
+ArcMode::ArcMode() {}
+
+void ArcMode::setTarget(float target_inches, Angle target_heading, float heading_mix_pct) {
+    this->target_deg = target_inches * (360.0 / (WHEEL_DIAM_INCHES * M_PI));
+    this->target_heading = target_heading;
+    this->heading_mix_pct = heading_mix_pct;
+    this->linear_speed = 0;
+    this->angular_speed = 0;
+    this->settle_count = 0;
+    this->settling_prev_left = 0;
+    this->settling_prev_right = 0;
+    this->settling_total_left = 0;
+    this->settling_total_right = 0;
+}
+
+void ArcMode::run(const DrivetrainState& drive_state, PID& linear_pid, PID& angular_pid) {
+    float curr_distance = (drive_state.left_degrees + drive_state.right_degrees) / 2.0;
+    float curr_distance_error = target_deg - curr_distance;
+    float curr_heading_error = Utils::getShortestAngleBetween(drive_state.heading, target_heading);
+    float prev_linear_speed = linear_speed;
+
+    linear_speed = linear_pid.getNextValue(curr_distance_error);
+    angular_speed = angular_pid.getNextValue(curr_heading_error);
+}
+
+bool ArcMode::hasSettled(const DrivetrainState& drive_state) {
+    float curr_left = drive_state.left_degrees;
+    float curr_right = drive_state.right_degrees;
+
+    settling_total_left += fabs(curr_left - settling_prev_left);
+    settling_total_right += fabs(curr_right - settling_prev_right);
+
+    if (settling_total_left < STRAIGHT_DRIVE_SETTLE_DEG_THRESHOLD &&
+        settling_total_right < STRAIGHT_DRIVE_SETTLE_DEG_THRESHOLD) {
+        settle_count++;
+    } else {
+        settle_count = 0;
+        settling_total_left = 0;
+        settling_total_right = 0;
+    }
+    settling_prev_left = curr_left;
+    settling_prev_right = curr_right;
+
+    return (settle_count > STRAIGHT_DRIVE_SETTLE_COUNT) ? true : false;
+}
+
+DriveSpeeds ArcMode::getSpeeds() {
+    return BlackMagic::Utils::desaturateSpeeds(linear_speed, angular_speed);
+}
+
+};
