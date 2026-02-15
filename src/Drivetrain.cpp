3f19ddbc666de7b37eb4172fad44914b75acb8d1
AVesich <75646434+AVesich@users.noreply.github.com>
2026-02-14 22:30:49 -0600

    renamed arcs

--- src/Drivetrain.cpp
+++ src/Drivetrain.cpp
@@ -112,11 +112,11 @@ void Drivetrain::driveArcSimple(float inches, Angle end_angle, ArcSettings arc_s
 }
 
 void Drivetrain::driveArcSimple(float inches, Angle end_angle, float linear_max_speed, float angular_max_speed, PID linear_pid, PID angular_pid) {
-    driveSimpleArc(inches, end_angle, { 0.0f, 0.001f }, linear_max_speed, angular_max_speed, linear_pid, angular_pid); // instant mix by default
+    driveArcSimple(inches, end_angle, { 0.0f, 0.001f }, linear_max_speed, angular_max_speed, linear_pid, angular_pid); // instant mix by default
 }
 
 void Drivetrain::driveArcSimple(float inches, Angle end_angle, PID linear_pid, PID angular_pid) {
-    driveSimpleArc(inches, end_angle, { 0.0f, 0.001f }, 100.0, 100.0, linear_pid, angular_pid); // instant mix by default
+    driveArcSimple(inches, end_angle, { 0.0f, 0.001f }, 100.0, 100.0, linear_pid, angular_pid); // instant mix by default
 }
 
 void Drivetrain::driveArcRadial(float radius_inches, Angle end_angle, ArcSettings arc_settings, float linear_max_speed, float angular_max_speed, PID linear_pid, PID angular_pid) {
@@ -134,7 +134,7 @@ void Drivetrain::driveArcRadial(float radius_inches, Angle end_angle, ArcSetting
 }
 
 void Drivetrain::driveArcRadial(float radius_inches, Angle end_angle, PID linear_pid, PID angular_pid) {
-    driveArc(radius_inches, end_angle, 100.0, 100.0, linear_pid, angular_pid);
+    driveArcRadial(radius_inches, end_angle, 100.0, 100.0, linear_pid, angular_pid);
 }
 
 void Drivetrain::driveCurve(float inches, std::vector<CurveKeyframe> keyframes, float linear_max_speed, float angular_max_speed, PID linear_pid, PID angular_pid) {
